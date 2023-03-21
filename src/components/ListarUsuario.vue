8a661d60e1e696eef66569339ac5a77d795e0ba1
MarcoDzCa23 <marcodzca@gmail.com>
2023-03-21 09:03:31 -0500

    Cambios insanos

--- src/components/ListarUsuario.vue
+++ src/components/ListarUsuario.vue
@@ -1,72 +1,102 @@
 <template>
-    <div>
-      <div class="card">
-        <div class="card-header">Articulos</div>
-  
-        <div class="card_body">
-          <table class="table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Proveedor</th>
-                <th>Precio</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="articulo in articulos" :key="articulo.id">
-                <td>{{ articulo.id }}</td>
-                <td>{{ articulo.name }}</td>
-                <td>{{ articulo.proveedor }}</td>
-                <td>{{ articulo.precio }}</td>
-                <td>
-                  <div class="btn-group" role="label" aria-label="">
-                    <!-- |<router-link :to="{name:'editar',param:{id:articulo.id}}" class="btn btn-info">Editar</router-link> | -->
-                    |<button
-                      type="button"
-                      v-on:click="borrarArticulo(articulo.id)"
-                      class="btn btn-danger"
-                    >
-                      Eliminar</button
-                    >|
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </template>
+  <div>
+    <div class="card">
+      <div class="card-header"><strong style="color:black;" >Lista de Usuarios</strong></div>
 
-  
+      <div class="card_body">
+        <!-- <button type="button" v-on:click="  editaregistro()" class="btn btn-outline-warning">Editar</button> -->
+        <table class="table">
+          
+          <thead>
+            <tr>
+              <th style="font-weight: bold;">Id</th>
+              <th style="font-weight: bold;">user</th>
+              <th style="font-weight: bold;">password</th>
+              <th style="font-weight: bold;">fechaRegistro</th>
+              <th style="font-weight: bold;">fkEmpleado</th>
+              <th style="font-weight: bold;">fkRol</th>
+              <th style="font-weight: bold;">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="usuario in Usuarios" :key="usuario.pkUsuario">
+              <td style="color:black;">{{ usuario.pkUsuario }}</td>
+              <td>{{ usuario.user }}</td>
+              <td>{{ usuario.password }}</td>
+              <td>{{ usuario.fechaRegistro }}</td>
+              <td>{{ usuario.fkEmpleado }}</td>
+              <td>{{ usuario.fkRol }}</td>
+              <td>
+                <div class="btn-group" role="label" aria-label="">
+                  <!-- |<router-link :to="{name:'editar',param:{id:articulo.id}}" class="btn btn-info">Editar</router-link> | -->
+                  <button type="button" v-on:click="borrarUsuario(usuario.pkUsuario)" class="btn btn-outline-danger">
+                    Eliminar</button>
+                    <button type="button" v-on:click="  editaregistro()" class="btn btn-outline-warning">Editar</button>        
+                </div>
+              </td>
+              
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
 <script>
 import axios from "axios";
+
+
 export default {
+
+  components:{
+
+  },
   data() {
     return {
-      articulos: [],
+      Usuarios: [],
+      Empleado: [],
+      nombresempleados: [{}],
+      smg: "",
     };
   },
   created: function () {
-    this.consultarArticulos();
+    this.consultarUsuario();
   },
   methods: {
-    consultarArticulos() {
-      axios.get("https://localhost:7053/articulos").then((result) => {
+    consultarUsuario() {
+      axios.get("https://localhost:7051/Usuario/").then((result) => {
         console.log(result.data);
-        this.articulos = result.data;
-      });
+        this.Usuarios = result.data.result;
+
+
+       });
+
+
     },
 
-    borrarArticulo(id) {
-      console.log(id);
+    borrarUsuario(id) {
+      var pregunta=window.confirm('Esta se seguro de eliminar este registro?');
 
-      axios.delete("https://localhost:7053/articulos/borrar/" + id);
+      if(pregunta===true){
+        axios.delete("https://localhost:7051/Usuario" + id);
+        window.location.href = "get";
+        
+      }
+        
+      
 
-      window.location.href = "Listar";
+    },
+    editaregistro() {
+    window.location.href="/putUsuario";
+
+    
+
     },
   },
+
 };
 </script>
