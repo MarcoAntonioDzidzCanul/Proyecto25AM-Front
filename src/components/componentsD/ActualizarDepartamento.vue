868218c18fc56b524923d35ba570a89b2d3cc8e0
Marco Antonio <Marcodzca@gmail.com>
2023-08-01 22:02:49 -0500

    Ya

--- src/components/componentsD/ActualizarDepartamento.vue
+++ src/components/componentsD/ActualizarDepartamento.vue
@@ -1,50 +1,52 @@
 <template>
     <div class="container-fluid">
-      <div class="card" style="background-color: #333333;">
-          <div class="card-header" style="background-color: #2E8B57; color: white;">Actualizar Departamento</div>
-          <div class="card-body">
-              <form v-on:submit.prevent="editarDep">
-                  <div class="form-group">
-                      <label for="nombre" style="color: white;">Nombre:</label>
-                              <input type="text" class="form-control" name="nombre" aria-describedby="helpId"
-                                  id="nombre" placeholder="Escriba el nombre" v-model="form.nombre" />
-                                  <br>
+        <div class="card" style="background-color: #333333;">
+            <div class="card-header" style="background-color: #2E8B57; color: white;">Actualizar Departamento</div>
+            <div class="card-body">
+                <form v-on:submit.prevent="editarDep">
+                    <div class="form-group">
+                        <label for="nombre" style="color: white;">Nombre:</label>
+                        <input type="text" class="form-control" name="nombre" aria-describedby="helpId" id="nombre"
+                            placeholder="Escriba el nombre" v-model="form.nombre" />
+                        <br>
 
-                                  <label for="direccion" style="color: white;">Direccion:</label>
-                              <input type="text" class="form-control" name="direccion" aria-describedby="helpId"
-                                  id="direccion" placeholder="Escriba la direccion" v-model="form.direccion" />
-                                  <br>
+                        <label for="direccion" style="color: white;">Direccion:</label>
+                        <input type="text" class="form-control" name="direccion" aria-describedby="helpId" id="direccion"
+                            placeholder="Escriba la direccion" v-model="form.direccion" />
+                        <br>
 
-                                  <label for="estado" style="color: white;">Estado:</label>
-                              <input type="text" class="form-control" name="estado" aria-describedby="helpId"
-                                  id="estado" placeholder="Escriba el estado" v-model="form.estado" />
-                                  <br>
+                        <label for="estado" style="color: white;">Estado:</label>
+                        <input type="text" class="form-control" name="estado" aria-describedby="helpId" id="estado"
+                            placeholder="Escriba el estado" v-model="form.estado" />
+                        <br>
 
-                                  <div class="form-group">
-    <label for="imagen" style="color: white;">Imagen:</label>
-    <input type="file" class="form-control" name="imagen" @change="handleImageChange">
-  </div>
+                        <div class="form-group">
+                            <label for="imagen" style="color: white;">Imagen:</label>
+                            <input type="file" class="form-control" name="imagen" @change="handleImageChange"
+                                v-on:change="form.ruta_imagen">
 
-                                  <label for="fk_Cliente" style="color: white;">fk_Cliente:</label>
-                              <input type="text" class="form-control" name="fk_Cliente" aria-describedby="helpId"
-                                  id="fk_Cliente" placeholder="Escriba el cliente" v-model="form.fk_Cliente" />
-                                  <br>
+                        </div>
 
-                              
+                        <label for="fk_Cliente" style="color: white;">fk_Cliente:</label>
+                        <input type="text" class="form-control" name="fk_Cliente" aria-describedby="helpId" id="fk_Cliente"
+                            placeholder="Escriba el cliente" v-model="form.fk_Cliente" />
+                        <br>
 
-                              
-                              </div><br/>
 
-                              <div class="btn-group" role="group">
-                                  <button type="submit" class="btn btn-outline-primary">Guardar</button>
-                                  <router-link :to="{name: 'listadepartamento'}" class="btn btn-outline-danger">Cancelar</router-link>
-                              </div>
-              </form>
 
-          </div>
-          </div>
-          </div>
- 
+
+                    </div><br />
+
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-outline-primary">Guardar</button>
+                        <router-link :to="{ name: 'listadepartamento' }"
+                            class="btn btn-outline-danger">Cancelar</router-link>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </template>
 <style>
 .card-body {
@@ -55,74 +57,72 @@
 import axios from 'axios'
 
 export default {
-  name: 'editardepartamento',
-  components: {
+    name: 'editardepartamento',
+    components: {
 
-  },
+    },
 
-  data() {
-      return {
-          pkDepartamento : null,
-          form: {
-              nombre : ""
-          },
-          
-          
-      }
-  },
-  mounted : function(){
-      this.pkDepartamento = this.$route.params.pkDepartamento;
-      console.log(this.pkDepartamento);
-      axios.get("https://localhost:7051/Departamento/" + this.pkDepartamento).then(datos =>
-      {
-          this.form.nombre = datos.data.result.nombre;
-          this.form.direccion = datos.data.result.direccion;
-          this.form.estado = datos.data.result.estado;
-          this.form.ruta_imagen = datos.data.result.ruta_imagen;
-          this.form.fk_Cliente = datos.data.result.fk_Cliente;
+    data() {
+        return {
+            pkDepartamento: null,
+            form: {
+                nombre: ""
+            },
 
-          console.log(this.form)
-      });
 
-  },
-  methods:{
-    handleImageChange(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+        }
+    },
+    mounted: function () {
+        this.pkDepartamento = this.$route.params.pkDepartamento;
+        console.log(this.pkDepartamento);
+        axios.get("https://localhost:7051/Departamento/" + this.pkDepartamento).then(datos => {
+            this.form.nombre = datos.data.result.nombre;
+            this.form.direccion = datos.data.result.direccion;
+            this.form.estado = datos.data.result.estado;
+            this.form.ruta_imagen = datos.data.result.ruta_imagen;
+            this.form.fk_Cliente = datos.data.result.fk_Cliente;
 
-    reader.onloadend = () => {
-      this.form.ruta_imagen = reader.result;
-    };
+            console.log(this.form)
+        });
 
-    if (file) {
-      reader.readAsDataURL(file);
-    } else {
-      this.form.ruta_imagen = null;
-    }
-  },
-      editarDep()
-      {
-          axios.put("https://localhost:7051/Departamento/" + this.pkDepartamento, this.form).then(data =>{
-              console.log(data);
-          });
-          this.$router.push("/listadepartamento")
-      }
-      
-  },
- 
+    },
+    methods: {
+        handleImageChange(event) {
+            const file = event.target.files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                this.form.ruta_imagen = reader.result;
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            } else {
+                this.form.ruta_imagen = null;
+            }
+        },
+        editarDep() {
+            axios.put("https://localhost:7051/Departamento/" + this.pkDepartamento, this.form).then(data => {
+                console.log(data);
+            });
+            this.$router.push("/listadepartamento")
+        }
+
+    },
+
 }
 
-  
-
- 
 
 
-          
 
 
-      
 
-  
+
+
+
+
+
+
 
 
 
--- src/components/componentsD/ListarDepartamento.vue
+++ src/components/componentsD/ListarDepartamento.vue
@@ -1,118 +1,121 @@
 <template>
-    <div>
-      <button type="button" v-on:click="crear()" class="btn btn-outline-success">Crear Departamento <i class="fa-solid fa-plus"></i></button> 
-      <div class="card">
-        <div class="card-header" style="background-color: #2E8B57;"><strong style="color:white;">Lista de Departamentos</strong></div>
-  
-        <div class="card_body" style="background-color: #2E8B57;">
-          <!-- <button type="button" v-on:click="  editaregistro()" class="btn btn-outline-warning">Editar</button> -->
-          <table class="table table-dark">
-            
-            <thead>
-              <tr>
-                <th style="font-weight: bold;">Id</th>
-                <th style="font-weight: bold;">Nombre</th>
-                <th style="font-weight: bold;">Direccion</th>
-                <th style="font-weight: bold;">Estado</th>
-                <th style="font-weight: bold;">Imagen</th>
-                <th style="font-weight: bold;">Cliente</th>
-                <th style="font-weight: bold;">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="dep in Departamentos" :key="dep.pkDepartamento">
-                <td style="color:black;">{{ dep.pkDepartamento }}</td>
-                <td>{{ dep.nombre }}</td>
-                <td>{{ dep.direccion }}</td>
-                <td>{{ dep.estado }}</td>
-                <td>
-      <img v-if="dep.ruta_imagen" :src="dep.ruta_imagen" alt="Imagen" style="width: 300px; height: auto;">
-      <span v-else>No se ha seleccionado imagen</span>
-    </td>
-                <td>{{ dep.fk_Cliente }}</td>
-                
-                <td>
-                  <div class="btn-group" role="label" aria-label="">
-                    <!-- |<router-link :to="{name:'editar',param:{id:articulo.id}}" class="btn btn-info">Editar</router-link> | -->                 
-                    <button type="button" v-on:click="eliminar(dep.pkDepartamento)" class="btn btn-outline-danger">Eliminar <i class="fa-solid fa-trash"></i></button>
-                    <button type="button" v-on:click="  editar(dep.pkDepartamento)" class="btn btn-outline-warning">Editar <i class="fa-solid fa-pen-to-square"></i></button>
-                    <!-- <button type="button" @click="mostrarFormulario(pkUsuario)" class="btn btn-outline-primary">Editar</button>  -->
-                      
-                    
-                             
-                  </div>
-                </td>
-                
-                
-              </tr>
-            </tbody>
-          </table>
-  
-        </div>
-  
-  
+  <div>
+    <button type="button" v-on:click="crear()" class="btn btn-outline-success">Crear Departamento <i
+        class="fa-solid fa-plus"></i></button>
+    <div class="card">
+      <div class="card-header" style="background-color: #2E8B57;"><strong style="color:white;">Lista de
+          Departamentos</strong></div>
+
+      <div class="card_body" style="background-color: #2E8B57;">
+        <!-- <button type="button" v-on:click="  editaregistro()" class="btn btn-outline-warning">Editar</button> -->
+        <table class="table table-dark">
+
+          <thead>
+            <tr>
+              <th style="font-weight: bold;">Id</th>
+              <th style="font-weight: bold;">Nombre</th>
+              <th style="font-weight: bold;">Direccion</th>
+              <th style="font-weight: bold;">Estado</th>
+              <th style="font-weight: bold;">Imagen</th>
+              <th style="font-weight: bold;">Cliente</th>
+              <th style="font-weight: bold;">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="dep in Departamentos" :key="dep.pkDepartamento">
+              <td style="color:black;">{{ dep.pkDepartamento }}</td>
+              <td>{{ dep.nombre }}</td>
+              <td>{{ dep.direccion }}</td>
+              <td>{{ dep.estado }}</td>
+              <td>
+                <img v-if="dep.ruta_imagen" :src="dep.ruta_imagen" alt="Imagen" style="width: 300px; height: auto;">
+                <span v-else>No se ha seleccionado imagen</span>
+              </td>
+              <td>{{ dep.fk_Cliente }}</td>
+
+              <td>
+                <div class="btn-group" role="label" aria-label="">
+                  <!-- |<router-link :to="{name:'editar',param:{id:articulo.id}}" class="btn btn-info">Editar</router-link> | -->
+                  <button type="button" v-on:click="eliminar(dep.pkDepartamento)" class="btn btn-outline-danger">Eliminar
+                    <i class="fa-solid fa-trash"></i></button>
+                  <button type="button" v-on:click="  editar(dep.pkDepartamento)" class="btn btn-outline-warning">Editar
+                    <i class="fa-solid fa-pen-to-square"></i></button>
+                  <!-- <button type="button" @click="mostrarFormulario(pkUsuario)" class="btn btn-outline-primary">Editar</button>  -->
+
+
+
+                </div>
+              </td>
+
+
+            </tr>
+          </tbody>
+        </table>
+
       </div>
+
+
     </div>
-    
-  </template>
+  </div>
+</template>
   
-  <script>
-  import axios from "axios";
-  
-  
-  export default {
-    name: 'listadepartamento',
-    components:{
-  
-    },
-    data() {
-      
-      return {
-        Departamentos: [
-       
+<script>
+import axios from "axios";
+
+
+export default {
+  name: 'listadepartamento',
+  components: {
+
+  },
+  data() {
+
+    return {
+      Departamentos: [
+
       ],
-        smg: "",
-      };
-    },
-    created: function () {
-      this.ListaDepartamento();
-    },
-    methods: {
-      ListaDepartamento() {
-        axios.get("https://localhost:7051/Departamento").then((result) => {
-          console.log(result.data);
-          this.Departamentos = result.data.result;
-  
-         });
-  
-      },
-  
-      eliminar(id) {
-        var pregunta=window.confirm('¿Desea eliminar el registro?');
-  
-        if(pregunta===true){
-          axios.delete("https://localhost:7051/Departamento?id=" + id);
-          window.location.href = "/listadepartamento";
-        }
-          
-        
-  
-      },
-      editar(pkDepartamento) {
-        console.log(pkDepartamento);
-      this.$router.push("/editardepartamento/"+ pkDepartamento)
-  
-      
-  
-      },
-      crear(){
-        window.location.href="/creardepartamento";
+      smg: "",
+    };
+  },
+  created: function () {
+    this.ListaDepartamento();
+  },
+  methods: {
+    ListaDepartamento() {
+      axios.get("https://localhost:7051/Departamento").then((result) => {
+        console.log(result.data);
+        this.Departamentos = result.data.result;
 
-      },
-    
+      });
 
-  
-    }
-      
-  };
-  </script>
+    },
+
+    eliminar(id) {
+      var pregunta = window.confirm('¿Desea eliminar el registro?');
+
+      if (pregunta === true) {
+        axios.delete("https://localhost:7051/Departamento?id=" + id);
+        window.location.href = "/listadepartamento";
+      }
+
+
+
+    },
+    editar(pkDepartamento) {
+      console.log(pkDepartamento);
+      this.$router.push("/editardepartamento/" + pkDepartamento)
+
+
+
+    },
+    crear() {
+      window.location.href = "/creardepartamento";
+
+    },
+
+
+
+  }
+
+};
+</script>
